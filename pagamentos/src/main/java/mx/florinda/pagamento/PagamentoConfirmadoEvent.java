b9193acf1ad0aa8bf3e211b717f32da35a657f2a
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2025-07-27 22:11:24 -0300

    producer no pagamentos service

---
+++ pagamentos/src/main/java/mx/florinda/pagamento/PagamentoConfirmadoEvent.java
@@ -0,0 +1,25 @@
+package mx.florinda.pagamento;
+
+import java.math.BigDecimal;
+
+public class PagamentoConfirmadoEvent {
+
+    public Long pagamentoId;
+    public Long pedidoId;
+    public BigDecimal valor;
+
+    public PagamentoConfirmadoEvent(Long pagamentoId, Long pedidoId, BigDecimal valor) {
+        this.pagamentoId = pagamentoId;
+        this.pedidoId = pedidoId;
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return "PagamentoConfirmadoEvent{" +
+                "pagamentoId=" + pagamentoId +
+                ", pedidoId=" + pedidoId +
+                ", valor=" + valor +
+                '}';
+    }
+}
